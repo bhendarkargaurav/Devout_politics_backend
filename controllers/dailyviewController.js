2b33339fe086a7e006c05acb6795e6da120db710
Gaurav <gaurav.bhe.24@gmail.com>
2025-06-17 14:41:04 +0530

    added

--- controllers/dailyviewController.js
+++ controllers/dailyviewController.js
@@ -1,14 +1,17 @@
-import DailyViews from "../model/dailyviewmodel.js";
+// import DailyViews from "../model/dailyviewmodel.js";
+import VideoStat from "../model/urlmodel.js"
  // find all dat afrom past also filter based on date and links
  
 export const getDailyViews = async (req, res) => {
   try {
     const filter = {};
+    
+    if (req.query.uploadDate) filter.uploadDate = req.query.uploadDate;
     if (req.query.date) filter.date = req.query.date;
     if (req.query.youtubelink) filter.youtubelink = req.query.youtubelink;
     if (req.query.facebooklink) filter.facebooklink = req.query.facebooklink;
 
-    const data = await DailyViews.find(filter);
+    const data = await VideoStat.find(filter);
     res.status(200).json({ success: true, data });
   } catch (error) {
     res.status(500).json({ success: false, error: error.message });
