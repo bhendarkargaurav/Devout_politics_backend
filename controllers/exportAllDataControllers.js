e6418f830bd679253b41bf6b925880deb2e0dda8
Gaurav <gaurav.bhe.24@gmail.com>
2025-06-20 17:09:59 +0530

    update views datevise

--- controllers/exportAllDataControllers.js
+++ controllers/exportAllDataControllers.js
@@ -67,6 +67,7 @@ import fs from "fs";
 import path from "path";
 import { Parser } from "json2csv";
 import VideoStat from "../model/urlmodel.js";
+import { getYoutubeViews, getFacebookViews } from "../utils/apiFetchers.js";
 
 export const getPaginatedVideos = async (req, res) => {
   const page = parseInt(req.query.page) || 1;
@@ -101,7 +102,7 @@ export const exportAllDataToCSV = async (req, res) => {
       facebookchannel: { $ne: "Unknown" },
     };
 
-    // get data based on data || date range
+    // get data based on date || date range
     const { uploadDate, startDate, endDate } = req.query;
     if (uploadDate) {
       const start = new Date(uploadDate);
@@ -157,5 +158,64 @@ export const exportAllDataToCSV = async (req, res) => {
   } catch (error) {
     console.error("Export CSV Error:", error);
     res.status(500).json({ error: "Failed to export video stats" });
+  }
+};
+
+
+
+
+
+export const updatedviewsbydate = async (req, res) => {
+   res.status(200).json({
+    success: true,
+    message: "Refresh Done",
+  });
+  try {
+    const { date } = req.query;
+    if (!date) {
+      return res.status(400).json({ error: "Date query param is required" });
+    }
+
+    const selectedDate = new Date(date);
+    const endOfDate = new Date(date);
+    endOfDate.setHours(23, 59, 59, 999);
+
+    //Find all records from that exact date
+    const oldRecords = await VideoStat.find({
+      uploadDate: {
+        $gte: selectedDate,
+        $lte: endOfDate,
+      },
+    });
+
+    if (oldRecords.length === 0) {
+      return res.status(404).json({ message: "No records found for this date" });
+    }
+
+    //Update each record with latest views
+    for (const record of oldRecords) {
+      const updatedYoutubeViews = await getYoutubeViews(record.youtubelink);
+      const updatedFacebookViews = await getFacebookViews(record.facebooklink);
+      const updatedTotalViews = updatedYoutubeViews + updatedFacebookViews;
+
+      await VideoStat.updateOne(
+        { _id: record._id },
+        {
+          $set: {
+            youtubeViews: updatedYoutubeViews,
+            facebookViews: updatedFacebookViews,
+            totalViews: updatedTotalViews,
+          },
+        }
+      );
+    }
+
+    res.json({
+      message: `Updated ${oldRecords.length} record(s) for date ${date}`,
+      updatedData: oldRecords,
+    });
+  } catch (error) {
+    console.error("View update error:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 };
