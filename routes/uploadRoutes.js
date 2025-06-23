81c8fcbc660976d152392ccf195a315558dfcc6f
Gaurav <gaurav.bhe.24@gmail.com>
2025-06-23 12:49:38 +0530

    setupr route

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,3 +1,30 @@
+// import express from "express";
+// import multer from "multer";
+// import { uploadCSV, deleteAlldata } from "../controllers/uploadControllers.js";//getAllLinks
+// import { getPaginatedVideos, exportAllDataToCSV, updatedviewsbydate  } from "../controllers/exportAllDataControllers.js";
+// import { getDailyViews } from "../controllers/dailyviewController.js";
+
+// //test
+// import { testuploadCSV } from "../controllers/testuploadController.js";
+
+// const router = express.Router();
+// const upload = multer({ dest: "uploads/" });
+
+// router.post("/upload-csv", upload.single("file"), uploadCSV);//uploadCSV
+// router.post("/delete-all", deleteAlldata);
+// router.get("/daily-views", getDailyViews);
+// router.get("/file-export", exportAllDataToCSV);
+// router.get("/paginate-data", getPaginatedVideos);
+// router.get("/refresh", updatedviewsbydate);
+// // router.get("/stats", getStats);
+// // router.get("/links", getAllLinks);
+
+// //testingRoute
+// router.post("/upload", upload.single("file"), testuploadCSV);
+
+// export default router;   
+
+
 import express from "express";
 import multer from "multer";
 import { uploadCSV, deleteAlldata } from "../controllers/uploadControllers.js";//getAllLinks
@@ -5,7 +32,7 @@ import { getPaginatedVideos, exportAllDataToCSV, updatedviewsbydate  } from "../
 import { getDailyViews } from "../controllers/dailyviewController.js";
 
 //test
-import { testuploadCSV } from "../controllers/testuploadController.js";
+import { testuploadCSV, getAllLinks, getYoutubeChannel, getFacebookChannel } from "../controllers/testuploadController.js";
 
 const router = express.Router();
 const upload = multer({ dest: "uploads/" });
@@ -17,7 +44,9 @@ router.get("/file-export", exportAllDataToCSV);
 router.get("/paginate-data", getPaginatedVideos);
 router.get("/refresh", updatedviewsbydate);
 // router.get("/stats", getStats);
-// router.get("/links", getAllLinks);
+router.get("/links", getAllLinks);
+router.get("/ytchannel", getYoutubeChannel);
+router.get("/fbchannel", getFacebookChannel);
 
 //testingRoute
 router.post("/upload", upload.single("file"),testuploadCSV)
