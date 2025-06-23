9b22c9b55dc74c3201d319e23701f9ef48bda550
Gaurav <gaurav.bhe.24@gmail.com>
2025-06-23 13:31:17 +0530

    router setup

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -25,6 +25,8 @@
 // export default router;   
 
 
+
+
 import express from "express";
 import multer from "multer";
 import { uploadCSV, deleteAlldata } from "../controllers/uploadControllers.js";//getAllLinks
@@ -37,18 +39,19 @@ import { testuploadCSV, getAllLinks, getYoutubeChannel, getFacebookChannel } fro
 const router = express.Router();
 const upload = multer({ dest: "uploads/" });
 
-router.post("/upload-csv", upload.single("file"), testuploadCSV );//uploadCSV
+router.post("/upload-csv", upload.single("file"), uploadCSV);//uploadCSV
 router.post("/delete-all", deleteAlldata);
 router.get("/daily-views", getDailyViews);
 router.get("/file-export", exportAllDataToCSV);
 router.get("/paginate-data", getPaginatedVideos);
 router.get("/refresh", updatedviewsbydate);
 // router.get("/stats", getStats);
+// router.get("/links", getAllLinks);
 router.get("/links", getAllLinks);
 router.get("/ytchannel", getYoutubeChannel);
 router.get("/fbchannel", getFacebookChannel);
 
 //testingRoute
-router.post("/upload", upload.single("file"),testuploadCSV)
+router.post("/upload", upload.single("file"), testuploadCSV);
 
 export default router;   
