b3f7efe900a07329e43f5c0af37b44083519a7ad
Gaurav <gaurav.bhe.24@gmail.com>
2025-06-26 12:55:56 +0530

    setup route

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -29,13 +29,13 @@
 
 import express from "express";
 import multer from "multer";
-import { uploadCSV, deleteAlldata } from "../controllers/uploadControllers.js";//getAllLinks
+import upload from "../middleware/multer.js"
+
+import { uploadCSV } from "../controllers/uploadControllers.js";
 import { getPaginatedVideos, exportAllDataToCSV, updatedviewsbydate  } from "../controllers/exportAllDataControllers.js";
 import { getDailyViews } from "../controllers/dailyviewController.js";
 import { getUploadStatus } from "../controllers/isUploadingController.js";
-import upload from "../middleware/multer.js"
-//test
-import { testuploadCSV, getAllLinks, getYoutubeChannel, getFacebookChannel } from "../controllers/testuploadController.js";
+import { testuploadCSV, getAllLinks, getYoutubeChannel, getFacebookChannel, deleteAlldata } from "../controllers/testuploadController.js";
 
 const router = express.Router();
 // const upload = multer({ dest: "uploads/" });
@@ -46,12 +46,9 @@ router.get("/daily-views", getDailyViews);
 router.get("/file-export", exportAllDataToCSV);
 router.get("/paginate-data", getPaginatedVideos);
 router.get("/refresh", updatedviewsbydate);
-// router.get("/stats", getStats);
-// router.get("/links", getAllLinks);
 router.get("/links", getAllLinks);
 router.get("/ytchannel", getYoutubeChannel);
 router.get("/fbchannel", getFacebookChannel);
-
 router.get("/isuploading", getUploadStatus);
 
 //testingRoute
