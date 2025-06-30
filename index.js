d48e98d11a5766edb9a0763920dc3691da723663
Gaurav <gaurav.bhe.24@gmail.com>
2025-06-30 14:27:13 +0530

    added

--- index.js
+++ index.js
@@ -14,12 +14,9 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 
-// import corsMiddleware from "./middleware/corsMiddleware.js";
-// app.use(corsMiddleware);
 app.use(cors({
   origin: '*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
 }));
 
 
@@ -36,16 +33,3 @@ mongoose
 
 
 
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// import app from "./app.js";
-
-// dotenv.config();
-
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => {
-//     console.log("Connected to MongoDB");
-//     app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
-//   })
-//   .catch((err) => console.error("MongoDB connection error:", err));
