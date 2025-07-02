e04dd6c179d36f925b5ed24779e31348cd2ef602
Gaurav <gaurav.bhe.24@gmail.com>
2025-07-02 22:39:37 +0530

    app.js

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-// app.js
-import express from "express";
-import cors from "cors";
-import uploadRoutes from "./routes/uploadRoutes.js";
-// import  corsMiddleware from "./middleware/corsMiddleware.js"
+// // app.js
+// import express from "express";
+// import cors from "cors";
+// import uploadRoutes from "./routes/uploadRoutes.js";
+// // import  corsMiddleware from "./middleware/corsMiddleware.js"
 
-const app = express();
+// const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
-// app.use(corsMiddleware);
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-}));
+// // app.use(corsMiddleware);
+// app.use(cors({
+//   origin: '*',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true,
+// }));
 
-app.use("/api", uploadRoutes);
+// app.use("/api", uploadRoutes);
 
-export default app;
+// export default app;
