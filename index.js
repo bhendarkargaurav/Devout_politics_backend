1ea050f1b7414537abaae50b4cbd7a8285375fe4
Gaurav <gaurav.bhe.24@gmail.com>
2025-07-15 17:37:43 +0530

    index.js, and package manager

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ import path from "path";
 import { fileURLToPath } from "url";
 import corsMiddleware from "./middleware/corsMiddleware.js"
 
+import { clerkMiddleware } from '@clerk/express'
+import { clerkClient, requireAuth, getAuth } from '@clerk/express'
+
 dotenv.config();
 
 const app = express();
@@ -15,12 +18,29 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 
+app.use(clerkMiddleware())
+
 app.use(corsMiddleware);
 
 // app.use(cors({
 //   origin: '*', 
 //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 // }));
+
+//test cleark
+// Use requireAuth() to protect this route
+// If user isn't authenticated, requireAuth() will redirect back to the homepage
+app.get('/protected', requireAuth(), async (req, res) => {
+  // Use `getAuth()` to get the user's `userId`
+  const { userId } = getAuth(req)
+
+  // Use Clerk's JavaScript Backend SDK to get the user's User object
+  const user = await clerkClient.users.getUser(userId)
+
+  return res.json({ user })
+})
+
+//complete with test cleark
 
 
 app.use("/api", uploadRoutes);
